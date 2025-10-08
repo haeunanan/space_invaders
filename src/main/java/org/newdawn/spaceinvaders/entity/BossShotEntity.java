0dfe984ed8b32484a4aecab8231e0a3b84f6b7ae
Jueun Lee <redsox20023095@gmail.com>
2025-10-08 19:18:11 +0900

    Add files via upload

---
+++ src/main/java/org/newdawn/spaceinvaders/entity/BossShotEntity.java
@@ -0,0 +1,34 @@
+package org.newdawn.spaceinvaders.entity;
+
+import org.newdawn.spaceinvaders.Game;
+
+public class BossShotEntity extends Entity {
+    private double moveSpeed = 250;
+    private Game game;
+
+    public BossShotEntity(Game game, String ref, int x, int y, double dx) {
+        super(ref, x, y);
+        this.game = game;
+        this.dx = dx; // 수평 이동 속도 (대각선 발사를 위해)
+        this.dy = moveSpeed; // 수직 이동 속도 (아래로)
+    }
+
+    @Override
+    public void move(long delta) {
+        super.move(delta);
+
+        // 화면 밖으로 나가면 자동 제거
+        if (y > 600) {
+            game.removeEntity(this);
+        }
+    }
+
+    @Override
+    public void collidedWith(Entity other) {
+        // 플레이어와 충돌 시
+        if (other instanceof ShipEntity) {
+            game.removeEntity(this); // 총알 제거
+            game.notifyDeath(); // 플레이어 사망 처리
+        }
+    }
+}
